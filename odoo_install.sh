12d2e7bc36a2f9942a0f1a50e3ed3ca852941cd5
Herry Gouw <herrygouw@gmail.com>
2020-08-01 17:33:30 +0700

    Update odoo_install.sh

--- odoo_install.sh
+++ odoo_install.sh
@@ -30,7 +30,7 @@ IS_ENTERPRISE="False"
 # Set this to True if you want to install Nginx!
 INSTALL_NGINX="False"
 # Set the superadmin password - if GENERATE_RANDOM_PASSWORD is set to "True" we will automatically generate a random password, otherwise we use this one
-OE_SUPERADMIN="Csd@8888"
+OE_SUPERADMIN="yourpwd"
 # Set to "True" to generate a random password, "False" to use the variable in OE_SUPERADMIN
 GENERATE_RANDOM_PASSWORD="False"
 OE_CONFIG="${OE_USER}-server"
